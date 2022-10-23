5342dc6456bb0b6d5a71f8d3cf6fda4df4f862b3
Edoardo Gennaretti <edo.gennaretti@gmail.com>
2022-10-23 20:29:51 +0200

    trying to implement a gui

--- Main_Test.java
+++ Main_Test.java
@@ -1,11 +1,54 @@
 package Labyrinth;
 
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
 import static Labyrinth.Constants.*;
 
 public class Main_Test {
     public static void main(String[] args){
-        Piece p = new Piece(false,true,false,true,CardinalPoint.SUD,Tresure.NONE);
-        p.revealTrap();
-        printMatrix(p.getMatrixPrintWithElementes());
+        JFrame f;
+
+        // JButton
+        JButton  b1, b2;
+        JLabel b;
+        // Label to display text
+        JLabel l;
+        //b.show();
+
+        f = new JFrame("panel");
+
+        // Creating a label to display text
+        l = new JLabel("panel label");
+
+        // Creating a new buttons
+        //b = new JLabel(new ImageIcon("tessere/0.png").getImage().getScaledInstance(20, 20,  java.awt.Image.SCALE_SMOOTH));
+        b1 = new JButton("button2");
+        b2 = new JButton("button3");
+
+        // Creating a panel to add buttons
+        JPanel p = new JPanel();
+
+        // Adding buttons and textfield to panel
+        // using add() method
+        p.add(b);
+        p.add(b1);
+        p.add(b2);
+        p.add(l);
+
+        // setbackground of panel
+        p.setBackground(Color.red);
+
+        // Adding panel to frame
+        f.add(p);
+
+        // Setting the size of frame
+        f.setSize(300, 300);
+
+        f.show();
     }
 }
